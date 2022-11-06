296263253989dcfb9031f48563d6dbdfb3e2a54f
Casa De Sanchez <casadesanchez@casas-mbp.lan>
2022-11-05 19:41:23 -0700

    added day 1 file

--- day_1.py
+++ day_1.py
@@ -4,13 +4,13 @@
 # python3 day_1.py
 # print("Hello World!")
 
-# excercise 1 print three seperate lines the same like this
+# exercise 1 print three seperate lines the same like this
 # print("Day 1 - Python Print Function")
 # print("The function is declared like this:")
 # print("print('what to print')")
 
 
-# excercise 2 debugging
+# exercise 2 debugging
 # print("Day 1 - String Manipulation")
 # When double quotes are in the function, single quotes relace where the double quotes usually go
 # print('String Concatenation is done with the "+" sign.')
@@ -18,3 +18,6 @@
 # print("New lines can be created with a backslash and n.")
 
 
+# exercise 3 - Input Function
+# this code will print the input and once you respond, it will print the coresponding integer 
+# print( len( input("What is your name? ") ) )
