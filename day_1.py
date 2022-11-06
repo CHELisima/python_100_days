2cf7acabd6e18e155e7150a0ee14c673cf6ceb2f
Casa De Sanchez <casadesanchez@casas-mbp.lan>
2022-11-05 18:30:10 -0700

    added day 1 file

--- day_1.py
+++ day_1.py
@@ -4,3 +4,10 @@
 # python3 day_1.py
 # print("Hello World!")
 
+# excercise 1 print three seperate lines the same like this
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+
+
+
