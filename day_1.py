9c41211f3e01c04bb9dadb21716e9430336a3749
Casa De Sanchez <casadesanchez@casas-mbp.lan>
2022-11-05 19:55:35 -0700

    added day 1 file

--- day_1.py
+++ day_1.py
@@ -21,3 +21,7 @@
 # exercise 3 - Input Function
 # this code will print the input and once you respond, it will print the coresponding integer 
 # print( len( input("What is your name? ") ) )
+
+# notes 
+# inside print() space then len()
+# inside len() space then input("") space before the closing ") and other clsoing )
