f422cb94a8ef432a080cb50fe091424d5fec5416
Chelsea Sanchez <chelsea.sanchez91@icloud.com>
2026-02-23 12:03:59 -0800

    test

--- day_1.py
+++ day_1.py
@@ -10,6 +10,7 @@
 # python3 day_1.py
 # print("Hello World!")
 
+
 # exercise 1 print three seperate lines the same like this
 # print("Day 1 - Python Print Function")
 # print("The function is declared like this:")
